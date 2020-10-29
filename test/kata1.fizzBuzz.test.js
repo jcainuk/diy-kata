adef5b2969968f3c2e20844fb746adfa8a7c3af7
Jonathan Cain <jonathan_m_cain@hotmail.co.uk>
2020-10-29 14:23:11 +0000

    Changed toEqual to toBe on test file

--- test/kata1.fizzBuzz.test.js
+++ test/kata1.fizzBuzz.test.js
@@ -1,11 +1,42 @@
 const { fizzBuzz } = require("../src");
 
 describe("fizzBuzz", () => {
-  test("returns Fizz when passed a multiple of 3", () => {});
+  test("returns Fizz when passed a multiple of 3", () => {
 
-  test("returns Buzz when passed a multiple of 5", () => {});
+    expect(fizzBuzz(3)).toBe("Fizz");
+    expect(fizzBuzz(6)).toBe("Fizz");
+    expect(fizzBuzz(9)).toBe("Fizz");
+    expect(fizzBuzz(12)).toBe("Fizz");
+    expect(fizzBuzz(18)).toBe("Fizz");
 
-  test("returns FizzBuzz when passed a multiple 3 and 5", () => {});
+});
 
-  test("returns the number when it isn't a multiple of 3 or 5", () => {});
+    test("returns Buzz when passed a multiple of 5", () => { 
+
+    expect(fizzBuzz(5)).toBe("Buzz");
+    expect(fizzBuzz(10)).toBe("Buzz");
+    expect(fizzBuzz(20)).toBe("Buzz");
+    expect(fizzBuzz(25)).toBe("Buzz");
+
+  });
+
+    test("returns FizzBuzz when passed a multiple 3 and 5", () => { 
+
+    expect(fizzBuzz(15)).toBe("FizzBuzz");
+    expect(fizzBuzz(30)).toBe("FizzBuzz");
+    expect(fizzBuzz(45)).toBe("FizzBuzz");
+
+  });
+
+    test("returns the number when it isn't a multiple of 3 or 5", () => { 
+
+    expect(fizzBuzz(1)).toBe(1);
+    expect(fizzBuzz(2)).toBe(2);
+    expect(fizzBuzz(4)).toBe(4);
+    expect(fizzBuzz(7)).toBe(7);
+    expect(fizzBuzz(8)).toBe(8);
+    expect(fizzBuzz(11)).toBe(11);
+    expect(fizzBuzz(13)).toBe(13);
+
+  });
 });
