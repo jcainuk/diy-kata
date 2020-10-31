a2f676dac8dfe4103bcc33cc73750f97fa59d3b9
Jonathan Cain <jonathan_m_cain@hotmail.co.uk>
2020-10-31 16:27:03 +0000

    wrote out my function for humanCatDogyears

--- src/kata4.humanCatDogYears.js
+++ src/kata4.humanCatDogYears.js
@@ -1,3 +1,40 @@
-const humanCatDogYears = number => {};
+const humanCatDogYears = number => {
+    // Cat Years
+    const catYears = function () {
+        // 15 cat years for first year
+        if (number === 1) {
+            return 15;
+        }
+    }
+
+    // +9 cat years for second year
+    else if (number === 2) {
+    return 24;
+}
+// +4 cat years for each year after that
+else if (number >= 3) {
+    return ((4 * (number - 2)) + 24);
+}
+    
+
+// Dog Years
+const dogYears = function () {
+
+    // 15 dog years for first year
+    if (number === 1) {
+        return 15;
+    }
+}
+    // +9 dog years for second year
+    else if (number === 2) {
+    return 24;
+}
+// +5 dog years for each year after that
+else if (number >= 3) {
+    return ((5 * (number - 2)) + 24);
+}
+
+
+};
 
 module.exports = humanCatDogYears;
