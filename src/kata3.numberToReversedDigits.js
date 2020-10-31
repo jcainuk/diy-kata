f3b819d237ec4d697bb45cd1b0233e3e6eaa6fc4
Jonathan Cain <jonathan_m_cain@hotmail.co.uk>
2020-10-31 15:40:00 +0000

    Removed the console log from kata 3

--- src/kata3.numberToReversedDigits.js
+++ src/kata3.numberToReversedDigits.js
@@ -4,13 +4,13 @@ const numberToReversedDigits = number => {
         //1. Convert 1234 into a string "1234"
         //AND Split the number 1234 into and array =>["1", "2", "3", "4"]
         const array = number.toString().split("");
-        console.log(array);
+        //console.log(array);
         //2.. Reverse said array ["4", "3", "2", "1"]
         const reversedArray = array.reverse();
-        console.log(reversedArray);
+        //console.log(reversedArray);
         //3. Change the array elements from strings to numbers =>[4, 3, 2, 1]
         const numberArray = reversedArray.map(Number);
-        console.log(numberArray);
+        //console.log(numberArray);
         return numberArray;
     }
     else {console.log('This data type is not a number');
