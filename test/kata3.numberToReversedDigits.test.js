69cb95101dc31cbe890e6629bc4f934fa6db2a09
Jonathan Cain <jonathan_m_cain@hotmail.co.uk>
2020-10-31 15:39:15 +0000

    Completed kata 1-3 and tests 1-3

--- test/kata3.numberToReversedDigits.test.js
+++ test/kata3.numberToReversedDigits.test.js
@@ -1,5 +1,16 @@
 const { numberToReversedDigits } = require("../src");
 
 describe("numberToReversedDigits", () => {
-  test("returns a reversed array of the number's digits", () => {});
+  test("returns a reversed array of the number's digits", () => {
+
+    expect(numberToReversedDigits(1234)).toEqual([4,3,2,1]);
+    expect(numberToReversedDigits(90876)).toEqual([6,7,8,0,9]);
+    expect(numberToReversedDigits(5643)).toEqual([3,4,6,5]);
+
+    expect(numberToReversedDigits('mocha')).toEqual('This data type is not a number');
+  
+
+
+
+  });
 });
