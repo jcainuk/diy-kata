3b6826ef55aa1dbc3ae802dbf46e03f20f605d0c
Jonathan Cain <jonathan_m_cain@hotmail.co.uk>
2020-11-01 13:21:11 +0000

    Completed my humanCatDogYears test js and ran it

--- test/kata4.humanCatDogYears.test.js
+++ test/kata4.humanCatDogYears.test.js
@@ -1,3 +1,16 @@
 const { humanCatDogYears } = require("../src");
 
+
+describe("humanCatDogYears", () => {
+    test("returns an array with human, cat and dogs years in that order", () => {
+  
+      expect(humanCatDogYears(0)).toEqual([0,0,0]);
+      expect(humanCatDogYears(1)).toEqual([1,15,15]);
+      expect(humanCatDogYears(2)).toEqual([2,24,24]);
+      expect(humanCatDogYears(3)).toEqual([3,28,29]);
+      expect(humanCatDogYears(24)).toEqual([24,112,134]);
+  
+  
+    });
+  });
 // Look Ma, no handlebars!!!
