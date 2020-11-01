8b36e1658350c22ee4576097e14bed968dd90dbc
Jonathan Cain <jonathan_m_cain@hotmail.co.uk>
2020-11-01 13:12:34 +0000

    Completed typing out my humanCatDogYears function

--- src/kata4.humanCatDogYears.js
+++ src/kata4.humanCatDogYears.js
@@ -1,48 +1,49 @@
 const humanCatDogYears = number => {
-    const catYears = function (number) {
+    //Declare variables to catch everything
+    let myArray = [];
+    let catYears;
+    let dogYears;
 
-        // 0 cat years 
-        if (number === 0) {
-            return 0;
-        }
-        // 15 cat years for first year
+    // 0 cat years & 0 dog years
+    if (number === 0) {
 
-        if (number === 1) {
-            return 15;
-        }
-        // +9 cat years for second year
-        if (number === 2) {
-            return 24;
-        }
+        catYears = 0;
+        console.log(catYears);
+        dogYears = 0;
+        console.log(dogYears);
+    }
+    // 15 cat years & 15 dog years for first year
+
+    else if (number === 1) {
+
+        catYears = 15;
+        console.log(catYears);
+        dogYears = 15;
+        console.log(dogYears);
+    }
+
+    // +9 cat years & dog years for second year
+    else if (number === 2) {
+
+        catYears = 24;
+        console.log(catYears);
+        dogYears = 24;
+        console.log(dogYears);
+    }
+
+    else if (number >= 3) {
+
         // +4 cat years for each year after that
-        if (number >= 3) {
-            return ((4 * (number - 2)) + 24);
-        }
-    }
-
-    // Dog Years
-    const dogYears = function (number) {
-
-        // 0 dog years 
-        if (number === 0) {
-            return 0;
-        }
-
-        // 15 dog years for first year
-        if (number === 1) {
-            return 15;
-        }
-
-        // +9 dog years for second year
-        if (number === 2) {
-            return 24;
-        }
+        catYears = ((4 * (number - 2)) + 24);
+        console.log(catYears);
         // +5 dog years for each year after that
-        if (number >= 3) {
-            return ((5 * (number - 2)) + 24);
-        }
+        dogYears = ((5 * (number - 2)) + 24);
+        console.log(dogYears);
     }
 
+    myArray.push(number, catYears, dogYears);
+    return myArray;
+    console.log(myArray);
 };
 
 module.exports = humanCatDogYears;
