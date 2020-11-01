f6ef1bfdc0ec84edb600c1e4219ccc3099863405
Jonathan Cain <jonathan_m_cain@hotmail.co.uk>
2020-11-01 14:06:26 +0000

    Wrote test for reachDestination and function

--- src/kata5.reachDestination.js
+++ src/kata5.reachDestination.js
@@ -1,3 +1,14 @@
-const reachDestination = (distance, speed) => {};
+const reachDestination = (distance, speed) => {
+        let distDivSpeed;
+
+    // We divide distance by speed (e.g.44 / 10) 
+    distDivSpeed = distance / speed;
+
+//The time 4.4 should be rounded up to the nearest 0.5 hours (half an hour).
+ let hoursToDestination = Math.round(distDivSpeed * 2) / 2;
+
+///It outputs a string `I should be there in ${4.5} hours` (four and a half hours)
+return `I should be there in ${hoursToDestination} hour(s)`;
+};
 
 module.exports = reachDestination;
--- test/kata5.reachDestination.test.js
+++ test/kata5.reachDestination.test.js
@@ -1,5 +1,17 @@
 const { reachDestination } = require("../src");
 
 describe("reachDestination", () => {
-  test("returns string with estimated time of arrival", () => {});
+  test("returns string with estimated time of arrival", () => {
+
+    expect(reachDestination(5.6,0.25)).toEqual("I should be there in 22.5 hour(s)");
+    expect(reachDestination(12,20)).toEqual("I should be there in 0.5 hour(s)");
+    expect(reachDestination(90,25)).toEqual("I should be there in 3.5 hour(s)");
+
+
+    expect(reachDestination(1000,0.01)).toEqual("I should be there in 100000 hour(s)");
+
+
+
+
+  });
 });
