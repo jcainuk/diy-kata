ad855c4d8fbea1f9f8b053630cc94e7163a26297
Jonathan Cain <jonathan_m_cain@hotmail.co.uk>
2020-11-02 13:31:48 +0000

    Created basic function structure for joinNames and some tests

--- src/kata6.joinNames.js
+++ src/kata6.joinNames.js
@@ -1,3 +1,15 @@
-const joinNames = namesObj => {};
+const joinNames = namesObj => {
+
+    let newString = namesObj.map(function (obj){
+    return obj.name;}).join(', ');
+    
+    let lastComma = newString.lastIndexOf(','), otherchar = ' & ';
+    
+    let finalString = newString.slice(0, lastComma) + otherchar + newString.slice(lastComma+1);
+    
+    return finalString;
+    
+        
+    };
 
 module.exports = joinNames;
--- test/kata6.joinNames.test.js
+++ test/kata6.joinNames.test.js
@@ -1,5 +1,18 @@
 const { joinNames } = require("../src");
 
 describe("joinNames", () => {
-  test("returns string of names, seperated by commas and an ampersand", () => {});
+  test("returns string of names, seperated by commas and an ampersand", () => {
+    //1 name
+    //expect(joinNames([{ name: 'Alvin' }])).toEqual('Alvin');
+    //2 name
+    expect(joinNames([{ name: 'Alvin' }, { name: 'Simon' }])).toEqual('Alvin & Simon');
+    //3 names
+    expect(joinNames([{ name: 'Bart' }, { name: 'Lisa' }, { name: 'Maggie' }])).toEqual('Bart, Lisa & Maggie');
+
+
+    //5 names 
+    expect(joinNames([{ name: 'Mel B' }, { name: 'Mel C' }, { name: 'Victoria' }, { name: 'Geri' }, { name: 'Emma' }])).toEqual('Mel B, Mel C, Victoria, Geri & Emma');
+
+
+  });
 });
