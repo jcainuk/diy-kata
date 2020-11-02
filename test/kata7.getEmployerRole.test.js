95209d28a08e9d769962722c3b23c145bf774431
Jonathan Cain <jonathan_m_cain@hotmail.co.uk>
2020-11-02 20:01:03 +0000

    Passed test of kata 7

--- test/kata7.getEmployerRole.test.js
+++ test/kata7.getEmployerRole.test.js
@@ -1,17 +1,21 @@
 const { getEmployerRole } = require("../src");
 
 describe("getEmployerRole", () => {
-
-  beforeAll( () => {
-    const employees = [ {name: 'Satti', role: 'Developer'},
-                    {name: 'Jenny', role: 'Sales Associate'},
-                    {name: 'Javid', role: 'Human Recommended Reading Assistant'}
-                  ];
-              });
-
-  test("returns the employee's role in the company", () => {
-
-    expect(getEmployerRole('Javid', employees).toEqual('Human Recommended Reading Assistant'));
-  });
-
+  it("returns the employee's role in the company", () => {});
+  expect(
+    getEmployerRole("Javid", [
+      {
+        name: "Satti",
+        role: "Developer"
+      },
+      {
+        name: "Jenny",
+        role: "Sales Associate"
+      },
+      {
+        name: "Javid",
+        role: "Human Recommended Reading Assistant"
+      }
+    ])
+  ).toEqual("Human Recommended Reading Assistant");
 });
