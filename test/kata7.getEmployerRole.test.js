8dd3176afcd0f17ec3a982dea765108ba05f94c8
Jonathan Cain <jonathan_m_cain@hotmail.co.uk>
2020-11-02 20:13:54 +0000

    Created more tests for kata 7

--- test/kata7.getEmployerRole.test.js
+++ test/kata7.getEmployerRole.test.js
@@ -1,21 +1,17 @@
 const { getEmployerRole } = require("../src");
 
 describe("getEmployerRole", () => {
+
+  const employees = [{name: 'Satti', role: 'Developer'},
+                     {name: 'Jenny', role: 'Sales Associate'},
+                     {name: 'Javid', role: 'Human Recommended Reading Assistant'}]
+
   it("returns the employee's role in the company", () => {});
-  expect(
-    getEmployerRole("Javid", [
-      {
-        name: "Satti",
-        role: "Developer"
-      },
-      {
-        name: "Jenny",
-        role: "Sales Associate"
-      },
-      {
-        name: "Javid",
-        role: "Human Recommended Reading Assistant"
-      }
-    ])
+  expect(getEmployerRole("Javid", employees)
   ).toEqual("Human Recommended Reading Assistant");
+  expect(getEmployerRole("Satti", employees)
+  ).toEqual("Developer");
+  expect(getEmployerRole("Jenny", employees)
+  ).toEqual("Sales Associate");
+
 });
