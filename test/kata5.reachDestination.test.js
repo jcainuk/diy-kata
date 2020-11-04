e6cfa359d456fa741c0524c3a65a38fbce07a192
Jonathan Cain <jonathan_m_cain@hotmail.co.uk>
2020-11-04 13:50:56 +0000

    Refactored kata 5 reachDestination by declaring distDivSpeed with const = distance /speed as it stays the same - then I added more negative tests using not.toBe

--- test/kata5.reachDestination.test.js
+++ test/kata5.reachDestination.test.js
@@ -4,14 +4,16 @@ describe("reachDestination", () => {
   test("returns string with estimated time of arrival", () => {
 
     expect(reachDestination(5.6,0.25)).toEqual("I should be there in 22.5 hour(s)");
+    expect(reachDestination(5.6,0.25)).not.toEqual("I should be there in 22 hour(s)");
+  
     expect(reachDestination(12,20)).toEqual("I should be there in 0.5 hour(s)");
-    expect(reachDestination(90,25)).toEqual("I should be there in 3.5 hour(s)");
+    expect(reachDestination(12,20)).not.toEqual("I should be there in 1 hour(s)");
 
+    expect(reachDestination(90,25)).toEqual("I should be there in 3.5 hour(s)");
+    expect(reachDestination(90,25)).not.toEqual("I should be there in 3 hour(s)");
 
     expect(reachDestination(1000,0.01)).toEqual("I should be there in 100000 hour(s)");
-
-
-
+    expect(reachDestination(1000,0.01)).not.toEqual("I should be there in 10000 hour(s)");
 
   });
 });
