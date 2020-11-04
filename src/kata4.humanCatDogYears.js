06d3d19cc98b8bd02ab440ee5bd85fb1a28e0e94
Jonathan Cain <jonathan_m_cain@hotmail.co.uk>
2020-11-04 13:43:39 +0000

    Refactored kata 4 into a switch statement and created more negative test scenarios

--- src/kata4.humanCatDogYears.js
+++ src/kata4.humanCatDogYears.js
@@ -1,49 +1,31 @@
 const humanCatDogYears = number => {
-    //Declare variables to catch everything
-    let myArray = [];
-    let catYears;
-    let dogYears;
-
-    // 0 cat years & 0 dog years
-    if (number === 0) {
-
-        catYears = 0;
-        console.log(catYears);
-        dogYears = 0;
-        console.log(dogYears);
+    //Set catYears and dogYears variable as 0 by default, 
+    let catYears = 0;
+    let dogYears = 0;
+    //Create switch statement for different cases
+    switch (number) {
+        //Case 0 human years
+        case 0:
+            catYears = dogYears = 0;
+            break;
+        //Case 1 human years (+15 years)
+        case 1:
+            catYears = dogYears = 15;
+            break;
+        //Case 2 human years (+ another 9 years for 2nd year)
+        case 2:
+            catYears = dogYears = 24;
+            break;
+        //Default case 3 or more human years
+        default:
+            // +4 cat years for each year after that
+            catYears = ((4 * (number - 2)) + 24);
+            // +5 dog years for each year after that
+            dogYears = ((5 * (number - 2)) + 24);
     }
-    // 15 cat years & 15 dog years for first year
-
-    else if (number === 1) {
-
-        catYears = 15;
-        console.log(catYears);
-        dogYears = 15;
-        console.log(dogYears);
-    }
-
-    // +9 cat years & dog years for second year
-    else if (number === 2) {
-
-        catYears = 24;
-        console.log(catYears);
-        dogYears = 24;
-        console.log(dogYears);
-    }
-
-    else if (number >= 3) {
-
-        // +4 cat years for each year after that
-        catYears = ((4 * (number - 2)) + 24);
-        console.log(catYears);
-        // +5 dog years for each year after that
-        dogYears = ((5 * (number - 2)) + 24);
-        console.log(dogYears);
-    }
-
-    myArray.push(number, catYears, dogYears);
-    return myArray;
-    console.log(myArray);
+    //Return an array of the 3 values
+    return [number, catYears, dogYears];
 };
+
 
 module.exports = humanCatDogYears;
--- test/kata4.humanCatDogYears.test.js
+++ test/kata4.humanCatDogYears.test.js
@@ -5,12 +5,22 @@ describe("humanCatDogYears", () => {
     test("returns an array with human, cat and dogs years in that order", () => {
   
       expect(humanCatDogYears(0)).toEqual([0,0,0]);
+     
+
       expect(humanCatDogYears(1)).toEqual([1,15,15]);
+
       expect(humanCatDogYears(2)).toEqual([2,24,24]);
+
       expect(humanCatDogYears(3)).toEqual([3,28,29]);
+      expect(humanCatDogYears(3)).not.toEqual([3,28,28]);
+      expect(humanCatDogYears(3)).not.toEqual([3,29,29]);
+      expect(humanCatDogYears(3)).not.toEqual([3,29,28]);
+
       expect(humanCatDogYears(24)).toEqual([24,112,134]);
-  
+      expect(humanCatDogYears(24)).not.toEqual([24,112,112]);
+      expect(humanCatDogYears(24)).not.toEqual([24,134,134]);
+      expect(humanCatDogYears(24)).not.toEqual([24,134,112]);
   
     });
   });
-// Look Ma, no handlebars!!!
+
